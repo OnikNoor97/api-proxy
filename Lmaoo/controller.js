bd5460d852d972bd084a047e9b146820e92009b2
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-10-29 21:43:00 +0000

    UserController Endpoint has been proxied

--- Lmaoo/controller.js
+++ Lmaoo/controller.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const httpProxy = require("http-proxy");
 
-router.use("/", (req, res) => {
-    res.json({ msg: "This is from the controller.js file ;) "})
+var proxy = httpProxy.createProxyServer(this.options);
+
+
+router.use("/User", (req, res) =>
+{
+    if (req.headers.authorization != "cojL6fOhdbO/rLg2JEWCng==") // Random one made for testing purposes
+    {
+        res.status(403).json({message: "Seems like your Access Token has expired, logout and login again!"})
+    }
+    else
+    {
+        proxy.web(req, res, { target: "http://localhost/www/MessAround/APITesting/index.php" })
+    }
+
 });
-
 
 module.exports = router;
