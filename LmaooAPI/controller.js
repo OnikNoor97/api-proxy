bc1b56f42dcf028b904a2b0566d2bd26d850154d
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-12-16 22:34:58 +0000

    LmaooAPI now uses Limiters

--- LmaooAPI/controller.js
+++ LmaooAPI/controller.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const router = express.Router();
 const httpProxy = require("http-proxy");
+const auth = require("../Auth");
 var proxy = httpProxy.createProxyServer(this.options);
 
 if (process.env.DEV_MODE) {
@@ -18,27 +19,15 @@ const projectControllerURL = `http://${server}/Workstation/${env}/lmaoo/Project/
 const ticketControllerURL = `http://${server}/Workstation/${env}/lmaoo/Ticket/ticketController.php`
 const adminControllerURL = `http://${server}/Workstation/${env}/lmaoo/Admin/adminController.php`
 
-router.use("/User", (req, res) => {
+router.use("/User", auth.limiter, auth.authSpeedLimiter, (req, res) => {
     res.status(400).json({ message: "This method has not been implemented yet" })
-    // if (req.headers["clientid"] == null || req.headers["clientid"]  == undefined) 
-    // {
-    //     res.status(400).json()
-    // }
-    // else if (req.headers.authorization != "cojL6fOhdbO/rLg2JEWCng==") // Random one made for testing purposes
-    // {
-    //     res.status(401).json({ message: "Not Authorized" })
-    // }
-    // else 
-    // {
-    //     proxy.web(req, res, { target: userControllerURL })
-    // }
 });
 
-router.use("/Ticket", (req, res) => {
+router.use("/Ticket", auth.limiter, auth.authSpeedLimiter, (req, res) => {
     res.status(400).json({ message: "This method has not been implemented yet" })
 })
 
-router.use("/User", (req, res) => {
+router.use("/User", auth.limiter, auth.authSpeedLimiter, (req, res) => {
     res.status(400).json({ message: "This method has not been implemented yet" })
 })
 
