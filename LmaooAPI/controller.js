a9a0e794521fd6b2accfa9a7d38bd8b702e385ac
Onik <18009030+OnikNoor97@users.noreply.github.com>
2020-12-16 10:50:55 +0000

    Restricted LmaooAPI till Lmaoo is finished

--- LmaooAPI/controller.js
+++ LmaooAPI/controller.js
@@ -19,18 +19,19 @@ const ticketControllerURL = `http://${server}/Workstation/${env}/lmaoo/Ticket/ti
 const adminControllerURL = `http://${server}/Workstation/${env}/lmaoo/Admin/adminController.php`
 
 router.use("/User", (req, res) => {
-    if (req.headers["clientid"] == null || req.headers["clientid"]  == undefined) 
-    {
-        res.status(400).json()
-    }
+    res.status(400).json({ message: "This method has not been implemented yet" })
+    // if (req.headers["clientid"] == null || req.headers["clientid"]  == undefined) 
+    // {
+    //     res.status(400).json()
+    // }
     // else if (req.headers.authorization != "cojL6fOhdbO/rLg2JEWCng==") // Random one made for testing purposes
     // {
     //     res.status(401).json({ message: "Not Authorized" })
     // }
-    else 
-    {
-        proxy.web(req, res, { target: userControllerURL })
-    }
+    // else 
+    // {
+    //     proxy.web(req, res, { target: userControllerURL })
+    // }
 });
 
 router.use("/Ticket", (req, res) => {
