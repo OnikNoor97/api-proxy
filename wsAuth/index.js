962594c3e1f47e25bd1ca93b8dfa23f7380048c6
Onik <18009030+OnikNoor97@users.noreply.github.com>
2021-03-18 11:03:16 +0000

    Made a start with index.js with socket IO

---
+++ wsAuth/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+
+const dbController = require("../Controller/DatabaseController");
+let db = new dbController("wsAuth");
+
+const io = require("../Controller/SocketioController");
+
+io.on("connection", socket => {
+
+    socket.on("message", async message => {
+        var query = "UPDATE session SET allowedAccess = 1 WHERE sessionToken = ?"
+        var parameter = ["bbc4ced2-2d35-49eb-85c3-ed8753651b46"];
+        let result = await db.InsertUpdateQuery(query, parameter);
+        io.emit("message", result);
+    });
+
+    async function middleware(req, res, next)
+    {
+        var query = "SELECT * FROM session WHERE sessionToken = ?"
+        var parameter = ["bbc4ced2-2d35-49eb-85c3-ed8753651b46"];
+        let result = await db.SelectQuery(query, parameter);
+        let { allowedAccess } = result[0];
+        
+        allowedAccess == 0 ? res.render("awaitConfirmation.html") : next();
+    }
+
+    router.use("/access", middleware, (req, res) => {
+        res.render("success.html"); 
+    })
+})
+
+
+
+router.use("/session/:sessionId", (req, res) => { 
+    res.render("button.html")
+})
+
+
+module.exports = router;
