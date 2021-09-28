e8fddae741769e7d973d5882a4e9265517c3787e
Onik Noor <O_Noor@live.co.uk>
2021-09-28 16:03:05 +0100

    Logs are always made, also logs can now be retrieved

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,12 @@ const APIError = require("./Core/APIError");
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// Log request coming in 
+app.use("*", async (req, res, next) => await LogController.createLog(req, res, next));
+
+// Get Logs from Server
+app.get("/admin/log", async (req, res, next) => await LogController.readLog(req, res, next));
+
 // Handling 404s 
 app.use("*", (req, res, next) => {
     return next(APIError.notFound());
