309b968c414f7d0d9bc9a0834032833a3d1dd888
Onik Noor <O_Noor@live.co.uk>
2021-10-03 14:59:45 +0100

    Added more validation on method

--- Server/Core/Library.js
+++ Server/Core/Library.js
@@ -6,7 +6,11 @@ module.exports = class Library {
 
         for(let i = 0; i < queryObj.values.length; i++) {
             if (typeof queryObj.values[i] === 'object') {
-                queryObj.values[i] = JSON.stringify(queryObj.values[i]);
+                // Replace object into string
+                queryObj.values[i] = JSON.stringify(queryObj.values[i])
+
+                // Escapes String to ensure no JSON Serialisation issues
+                queryObj.values[i] = queryObj.values[i].replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
             }
             query = query.replace(`$${i+1}`.trim(), `'${queryObj.values[i]}'`);
         }
