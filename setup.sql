5021e1ace342c55562c2a33ede23ac5ec9eb71ff
Onik Noor <O_Noor@live.co.uk>
2021-10-03 14:58:54 +0100

    Updated Setup SQL

--- setup.sql
+++ setup.sql
@@ -5,10 +5,25 @@ USE `APIProxy`;
 -- Dumping structure for table APIProxy.Log
 CREATE TABLE IF NOT EXISTS `Log` (
   `LogId` int NOT NULL AUTO_INCREMENT,
+  `method` varchar(5) DEFAULT NULL,
+  `hostname` varchar(50) DEFAULT NULL,
   `sourceIP` varchar(50) DEFAULT NULL,
-  `destinationIP` varchar(50) DEFAULT NULL,
   `url` varchar(50) DEFAULT NULL,
   `headers` json DEFAULT NULL,
+  `queryParams` json DEFAULT NULL,
   `body` json DEFAULT NULL,
   PRIMARY KEY (`LogId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=armscii8;
+) ENGINE=InnoDB AUTO_INCREMENT=234 DEFAULT CHARSET=armscii8;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table APIProxy.Proxy
+CREATE TABLE IF NOT EXISTS `Proxy` (
+  `proxyId` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET armscii8 COLLATE armscii8_general_ci NOT NULL,
+  `proxyUrl` varchar(100) CHARACTER SET armscii8 COLLATE armscii8_general_ci NOT NULL,
+  `proxyIp` varchar(20) CHARACTER SET armscii8 COLLATE armscii8_general_ci NOT NULL,
+  `proxyPort` int NOT NULL DEFAULT '80',
+  `isService` varchar(1) CHARACTER SET armscii8 COLLATE armscii8_general_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`proxyId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=armscii8;
