d41560c3f8f51f95ec0a5a727d6651d022b73e10
Onik Noor <O_Noor@live.co.uk>
2021-10-03 14:59:23 +0100

    Proxy DTO implemented

---
+++ Server/Dto/ProxyDto.js
@@ -0,0 +1,21 @@
+const Library = require("../Core/Library");
+const BaseDto = require("./BaseDto");
+
+module.exports = class ProxyDto extends BaseDto {
+    constructor() {
+        super();
+        this.tableName = "Proxy"
+        this.columns = ["proxyId", "name", "proxyUrl", "proxyIp", "proxyPort", "isService"];
+        return this;
+    }
+
+    async create(values) {
+        let query = Library.queryObjectToQuery(this.sql.insert(this.tableName, values));
+        return await this.db.create(query);
+    }
+
+    async read(conditions) {
+        let query = Library.queryObjectToQuery(this.sql.select(this.tableName, this.columns).where(conditions));
+        return await this.db.read(query)
+    }
+}
