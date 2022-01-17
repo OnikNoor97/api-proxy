9196b86ef6c1845413b72f1eb52e307122245646
tufanbutuner <tufanbutuner@gmail.com>
2022-01-17 00:11:13 +0000

    Updated modal component

--- src/components/atoms/Table/Modal/ModalComponent.tsx
+++ src/components/atoms/Table/Modal/ModalComponent.tsx
@@ -48,76 +48,75 @@ export default function ModalComponent() {
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <RiEditLine
-                      className="h-6 w-6 text-red-600"
-                      aria-hidden="true"
-                    />
-                  </div>
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <Dialog.Title
                       as="h3"
-                      className="text-lg leading-6 font-medium text-gray-900"
+                      className="text-lg leading-6 font-medium text-gray-900 mb-10"
                     >
                       Edit Proxy Host
                     </Dialog.Title>
 
-                    <ul
-                      className="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4"
-                      id="tabs-tabFill"
-                      role="tablist"
-                    >
-                      <li
-                        className="nav-item flex-auto text-center"
-                        role="presentation"
-                      >
-                        <a
-                          href="#tabs-home"
-                          className="nav-link w-full block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active underline"
-                          id="tabs-home-tab"
-                          data-bs-toggle="pill"
-                          data-bs-target="#tabs-home"
-                          role="tab"
-                          aria-controls="tabs-home"
-                          aria-selected="true"
-                        >
-                          Details
-                        </a>
-                      </li>
-                    </ul>
-                    <div className="tab-content" id="tabs-tabContent">
-                      <div
-                        className="tab-pane fade show active"
-                        id="tabs-home"
-                        role="tabpanel"
-                        aria-labelledby="tabs-home-tab"
-                      >
-                        <h3>Domain name</h3>
-                        <input
-                          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                          id="inline-full-name"
-                          type="text"
-                          value="Jane Doe"
-                        />
+                    <div className="mb-8">
+                      <h3>Domain name</h3>
+                      <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-full-name"
+                        type="text"
+                      />
+                    </div>
+
+                    <form className="w-full max-w-lg">
+                      <div className="flex flex-wrap -mx-3 mb-2">
+                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Scheme
+                          </label>
+                          <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-city"
+                            type="text"
+                            placeholder="http"
+                            required
+                          />
+                        </div>
+
+                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Fwd Hostname / IP
+                          </label>
+                          <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-zip"
+                            type="text"
+                            placeholder="authelia"
+                            required
+                          />
+                        </div>
+
+                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Forward Port
+                          </label>
+                          <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-zip"
+                            type="text"
+                            placeholder="9091"
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="mt-5">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to deactivate your account? All of
-                        your data will be permanently removed. This action
-                        cannot be undone.
-                      </p>
-                    </div>
+                    </form>
                   </div>
                 </div>
               </div>
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={() => setOpen(false)}
                 >
-                  Deactivate
+                  Save
                 </button>
                 <button
                   type="button"
