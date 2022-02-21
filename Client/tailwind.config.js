27646dcbd4286ba4fb3ebd4751708f1a5a4a1735
Onik Noor <O_Noor@live.co.uk>
2022-02-21 13:46:08 +0000

    Cleanup - Rename from src to Client

---
+++ Client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./Client/pages/**/*.{js,ts,jsx,tsx}",
+    "./Client/components/**/*.{js,ts,jsx,tsx}",
+    "./Client/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
